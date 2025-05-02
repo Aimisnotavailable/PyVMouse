4793f25b3c1ba49d1acfd738c8fbd85708c5280b
Aimisnotavailable <aim.990111@gmail.com>
2025-05-02 20:50:42 +0800

    Updated Movement

--- cam_test.py
+++ cam_test.py
@@ -1,22 +1,36 @@
 import cv2
 import mediapipe as mp
+import pyautogui as pg
+
+from screeninfo import get_monitors
+
+TRACK_IDX = 8
+SENSITIVITY = 5
 
 # Initialize MediaPipe Hands module
 mp_hands = mp.solutions.hands
-hands = mp_hands.Hands()
+hands = mp_hands.Hands(max_num_hands=2)
 mp_draw = mp.solutions.drawing_utils
 
+last_position = [0, 0]
+
 # Start OpenCV video capture
-cap = cv2.VideoCapture("manny.mp4")  # 0 for default webcam
-frame_width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-frame_height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+cap = cv2.VideoCapture(0)  # 0 for default webcam
+
+
+for m in get_monitors():
+    frame_width = m.width
+    frame_height = m.height
+
+cursor_position = (frame_width // 2, frame_height// 2)
 
 while cap.isOpened():
     ret, frame = cap.read()
     if not ret:
         break
     
-    print(frame_height, frame_width)
+    
+
     # Convert frame to RGB (required by MediaPipe)
     rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
 
@@ -27,7 +41,18 @@ while cap.isOpened():
     if results.multi_hand_landmarks:
         for hand_landmarks in results.multi_hand_landmarks:
             mp_draw.draw_landmarks(frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+            #print(hand_landmarks)
+        
+        # print(min(frame_width, (1-results.multi_hand_landmarks[0].landmark[TRACK_IDX].x) * frame_width * SENSITIVITY), 
+        #           min(frame_height, results.multi_hand_landmarks[0].landmark[TRACK_IDX].y * frame_height * SENSITIVITY))
+        curr_pos = [(1-results.multi_hand_landmarks[0].landmark[TRACK_IDX].x) * frame_width, 
+                         results.multi_hand_landmarks[0].landmark[TRACK_IDX].y * frame_height]
+        movement = [(last_position[0] - curr_pos[0]) * SENSITIVITY, (last_position[1] - curr_pos[1]) * SENSITIVITY]
+        cursor_position = [max(5, min(frame_width - 5, cursor_position[0] + movement[0])), max(5, min(frame_height - 5, cursor_position[1] - movement[1]))]
+        last_position = curr_pos.copy()
 
+        print(cursor_position)
+        pg.moveTo(cursor_position[0], cursor_position[1], 0.01)
     # Show the frame
     cv2.imshow("MediaPipe Hand Tracking", frame)
 
